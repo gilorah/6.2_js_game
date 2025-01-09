66c64f0d0f2b801cec230bd0ee2361fd80f2b4ba
gilorah <fabrigilorah@hotmail.com>
2025-01-09 23:04:27 +0100

    progress pt2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,134 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
+    const button1 = document.getElementById(".button");
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let startPoint = 150
+    let doodlerBottomSpace = startPoint
     let platformCount = 5
+    let platforms = []
+    let UpTimerId
+    let downTimerId
+    let isJumping = true
+    let isGoingLeft = false
+    let isGoingRight = false
+    let leftTimerId
+    let rightTimerId
 
-    function createDoodler (){
+    function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
 
-    class Platform{
-       constructor(newPlatBottom){
-        this.bottom = newPlatBottom
-        this.left = Math.random() * 315
-        this.visual = document.createElement('div')
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
 
-        const visual = this.visual
-       }
-    }
-
-function createPlatforms(){
-    for (let i=0; i<platformCount; i++){
-         let platformSpace = 600 / platformCount
-         let newPlatBottom = 100 + i * platformSpace
-         let newPlatform = new Platform(newPlatBottom)
-    }
-}
-
-    function start(){
-        if (!isGameOver){
-            createDoodler()
-            createPlatforms()
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
         }
     }
-//make Homescreen and attach to a button
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++) {
+            let platformSpace = 600 / platformCount
+            let newPlatBottom = 100 + i * platformSpace
+            let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
+        }
+    }
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        clearInterval; (downTimerId)
+        isJumping = true
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > startPoint + 200) {
+                fall()
+            }
+        }, 30)
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        isJumping = false
+        downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
+            platforms.forEach(platform => {
+                if (
+                    (doodlerBottomSpace >= platform.bottom) &&
+                    (doodlerBottomSpace <= platform.bottom + 15) &&
+                    ((doodlerLeftSpace + 60) >= platform.left) &&
+                    (doodlerLeftSpace <= (platform.left + 85)) &&
+                    !isJumping
+                ) {
+                    console.log('landed')
+                    startPoint = doodlerBottomSpace
+                    jump()
+                }
+            })
+        }, 30)
+    }
+
+    function controls(e) {
+        if (e.key === "ArrowLeft") {
+            moveLeft()
+        } else if (e.key === "ArrowRight") {
+            //move right
+        } else if (e.key === "ArrowUp") {
+            // moveStraight
+        }
+    }
+
+    function moveLeft(){
+     isGoingLeft = true
+     leftTimerId = setInterval(function(){
+       doodlerLeftSpace -=5
+       doodler.style.left = doodlerLeftSpace + 'px'
+     },30)
+    }
+    
+    function gameOver() {
+        console.log('game over')
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+
+    function start() {
+        if (!isGameOver) {
+            createPlatforms()
+            createDoodler()
+            setInterval(movePlatforms, 30)
+            jump()
+            document.addEventListener('keyup',control)
+        }
+    }
+
+    //make Homescreen and attach to a button
     start()
 })
